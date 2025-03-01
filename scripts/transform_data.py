d5777f2e4e874bc4b5ec1b86e9cb46f21a9626d4
diego-ramos98 <diego.ramos.fortunato@outlook.com>
2025-03-01 10:56:27 -0300

    transformando e salvado dados em csv

--- scripts/transform_data.py
+++ scripts/transform_data.py
@@ -6,7 +6,7 @@ import extract_and_save_data
 
 
 
-#client = extract_and_save_data.connect_mongo_db()
+
 
 
 #lendo dados do banco
@@ -36,26 +36,49 @@ def read_collection_to_list(collection_name):
     
 
 # DE LISTA PARA DATA FRAME
-def transform_list_to_data_frame(data_frame,list_data):
+def transform_list_to_data_frame(list_data):
     data_frame = pd.DataFrame(list_data)
     return data_frame
 
-#Conferindo os diferentes municipios registrado
-def retrive_unique_municipios(data_frame):
-    return data_frame["municipio"].unique
 
 
 #Pegando apenas os dados do municipio do rio
 
-def retrive_data_municipios_rj():
+def retrive_data_municipios_rj(collection):
     query = {"municipio" : "RIO DE JANEIRO"}
     
     lista_dados_eleitorais_rj = []
 
     for docs in collection.find(query):
+        
         lista_dados_eleitorais_rj.append(docs)
-    
+
     return lista_dados_eleitorais_rj
 
 
+
+def drop_columns(data_frame):
+    data_frame = data_frame.drop(["numLocal","numZona","secoes"],axis=1)
+    return data_frame
+
+
+
 # Transformando em csv
+def data_frame_to_csv(path,data_frame):
+    data_frame.to_csv(path)
+
+
+
+if __name__ == "__main__":
+    client = extract_and_save_data.connect_mongo_db("mongodb://localhost:27017/")
+    db = retrieve_data_db(client,"db_dados_eleitorais")
+    collection = retrieve_collection(db,"locais_votacoes")
+    list_data = read_collection_to_list(collection)
+    data_frame = transform_list_to_data_frame(list_data)
+    list_municipios_rj = retrive_data_municipios_rj(collection)
+    df_municipios_rj = transform_list_to_data_frame(list_municipios_rj)
+    df_municipios_rj = drop_columns(df_municipios_rj)
+    data_csv = data_frame_to_csv("../data/dados_eleitorais_rj2.csv",df_municipios_rj)
+    client.close()
+
+
